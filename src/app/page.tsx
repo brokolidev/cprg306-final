62e3c7be31276bae9e7e80f15cb41c34bdfc6772
brokolidev <bocalist@gmail.com>
2024-11-28 02:05:42 -0700

    added default templates and layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+import { Avatar } from '@/components/avatar'
+import { Badge } from '@/components/badge'
+import { Divider } from '@/components/divider'
+import { Heading, Subheading } from '@/components/heading'
+import { Select } from '@/components/select'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/table'
+import { getRecentOrders } from '@/data'
 
-export default function Home() {
+export function Stat({ title, value, change }: { title: string; value: string; change: string }) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div>
+        <Divider />
+        <div className="mt-6 text-lg/6 font-medium sm:text-sm/6">{title}</div>
+        <div className="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{value}</div>
+        <div className="mt-3 text-sm/6 sm:text-xs/6">
+          <Badge color={change.startsWith('+') ? 'lime' : 'pink'}>{change}</Badge>{' '}
+          <span className="text-zinc-500">from last week</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+  )
+}
+
+export default async function Home() {
+  let orders = await getRecentOrders()
+
+  return (
+      <>
+        <Heading>Good afternoon, Erica</Heading>
+        <div className="mt-8 flex items-end justify-between">
+          <Subheading>Overview</Subheading>
+          <div>
+            <Select name="period">
+              <option value="last_week">Last week</option>
+              <option value="last_two">Last two weeks</option>
+              <option value="last_month">Last month</option>
+              <option value="last_quarter">Last quarter</option>
+            </Select>
+          </div>
+        </div>
+        <div className="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
+          <Stat title="Total revenue" value="$2.6M" change="+4.5%" />
+          <Stat title="Average order value" value="$455" change="-0.5%" />
+          <Stat title="Tickets sold" value="5,888" change="+4.5%" />
+          <Stat title="Pageviews" value="823,067" change="+21.2%" />
+        </div>
+        <Subheading className="mt-14">Recent orders</Subheading>
+        <Table className="mt-4 [--gutter:theme(spacing.6)] lg:[--gutter:theme(spacing.10)]">
+          <TableHead>
+            <TableRow>
+              <TableHeader>Order number</TableHeader>
+              <TableHeader>Purchase date</TableHeader>
+              <TableHeader>Customer</TableHeader>
+              <TableHeader>Event</TableHeader>
+              <TableHeader className="text-right">Amount</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+                <TableRow key={order.id} href={order.url} title={`Order #${order.id}`}>
+                  <TableCell>{order.id}</TableCell>
+                  <TableCell className="text-zinc-500">{order.date}</TableCell>
+                  <TableCell>{order.customer.name}</TableCell>
+                  <TableCell>
+                    <div className="flex items-center gap-2">
+                      <Avatar src={order.event.thumbUrl} className="size-6" />
+                      <span>{order.event.name}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-right">US{order.amount.usd}</TableCell>
+                </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </>
+  )
 }
