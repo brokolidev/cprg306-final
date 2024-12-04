6b419750029cac6c16473d151a590e9dc3a31834
brokoli <bocalist@gmail.com>
2024-12-04 13:50:27 -0700

    updated greeting based on time and logged in user name

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -5,6 +5,7 @@ import { Divider } from '@/components/divider'
 import { Heading, Subheading } from '@/components/heading'
 import { Select } from '@/components/select'
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/table'
+import {useAuth} from "@/hooks/auth";
 
 export function Stat({ title, value, change }: { title: string; value: string; change: string }) {
   return (
@@ -21,11 +22,24 @@ export function Stat({ title, value, change }: { title: string; value: string; c
 }
 
 export default function Home() {
-  // let orders = await getRecentOrders()
+  const { user } = useAuth({ middleware: 'auth' })
+
+  const greeting = () => {
+    const now = new Date(); // Get the current date and time
+    const hour = now.getHours(); // Extract the current hour (0-23)
+
+    if (hour >= 5 && hour < 12) {
+      return "morning"; // 5:00 AM to 11:59 AM
+    } else if (hour >= 12 && hour < 18) {
+      return "afternoon"; // 12:00 PM to 5:59 PM
+    } else {
+      return "evening"; // 6:00 PM to 4:59 AM
+    }
+  }
 
   return (
     <>
-      <Heading>Good afternoon, Erica</Heading>
+      <Heading>Good {greeting()}, {user?.first_name && user.first_name}</Heading>
       <div className="mt-8 flex items-end justify-between">
         <Subheading>Overview</Subheading>
         <div>
