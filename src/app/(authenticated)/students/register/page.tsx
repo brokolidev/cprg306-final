c5d22c1f5719043daf3017a9a82fefe0b28d8a64
brokolidev <bocalist@gmail.com>
2024-12-04 23:19:26 -0700

    added student registration form

--- src/app/(authenticated)/students/register/page.tsx
+++ src/app/(authenticated)/students/register/page.tsx
@@ -1,12 +1,9 @@
 import { Button } from '@/components/button'
-import { Checkbox, CheckboxField } from '@/components/checkbox'
 import { Divider } from '@/components/divider'
-import { Label } from '@/components/fieldset'
 import { Heading, Subheading } from '@/components/heading'
 import { Input } from '@/components/input'
-import { Select } from '@/components/select'
-import { Text } from '@/components/text'
-import { Textarea } from '@/components/textarea'
+import { Link } from '@/components/link'
+import { ChevronLeftIcon } from '@heroicons/react/16/solid'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -15,80 +12,70 @@ export const metadata: Metadata = {
 
 export default function StudentRegisterPage() {
   return (
-    <form method="post" className="mx-auto max-w-4xl">
-      <Heading>Settings</Heading>
-      <Divider className="my-10 mt-6" />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Name</Subheading>
-          <Text>This will be displayed on your public profile.</Text>
-        </div>
-        <div>
-          <Input aria-label="Organization Name" name="name" defaultValue="Catalyst" />
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Bio</Subheading>
-          <Text>This will be displayed on your public profile. Maximum 240 characters.</Text>
-        </div>
-        <div>
-          <Textarea aria-label="Organization Bio" name="bio" />
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Organization Email</Subheading>
-          <Text>This is how customers can contact you for support.</Text>
-        </div>
-        <div className="space-y-4">
-          <Input type="email" aria-label="Organization Email" name="email" defaultValue="info@example.com" />
-          <CheckboxField>
-            <Checkbox name="email_is_public" defaultChecked />
-            <Label>Show email on public profile</Label>
-          </CheckboxField>
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Address</Subheading>
-          <Text>This is where your organization is registered.</Text>
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div className="space-y-1">
-          <Subheading>Currency</Subheading>
-          <Text>The currency that your organization will be collecting.</Text>
-        </div>
-        <div>
-          <Select aria-label="Currency" name="currency" defaultValue="cad">
-            <option value="cad">CAD - Canadian Dollar</option>
-            <option value="usd">USD - United States Dollar</option>
-          </Select>
-        </div>
-      </section>
-
-      <Divider className="my-10" soft />
-
-      <div className="flex justify-end gap-4">
-        <Button type="reset" plain>
-          Reset
-        </Button>
-        <Button type="submit">Save changes</Button>
+    <>
+      <div className="max-lg:hidden">
+        <Link href="/Students" className="inline-flex items-center gap-2 text-sm/6 text-zinc-500 dark:text-zinc-400">
+          <ChevronLeftIcon className="size-4 fill-zinc-400 dark:fill-zinc-500" />
+          Students
+        </Link>
       </div>
-    </form>
+      <form method="post" className="mt-4 lg:mt-8">
+        <Heading>Student Registration</Heading>
+        <Divider className="my-10 mt-6" />
+
+        <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div className="space-y-1">
+            <Subheading>First Name</Subheading>
+          </div>
+          <div>
+            <Input aria-label="First Name" name="first_name" placeholder="John" />
+          </div>
+          <div className="space-y-1">
+            <Subheading>Last Name</Subheading>
+          </div>
+          <div>
+            <Input aria-label="Last Name" name="last_name" placeholder="Doe" />
+          </div>
+          <div className="space-y-1">
+            <Subheading>Email</Subheading>
+          </div>
+          <div className="space-y-4">
+            <Input type="email" aria-label="Email" name="email" placeholder="info@example.com" />
+          </div>
+          <div className="space-y-1">
+            <Subheading>Date of Birth</Subheading>
+          </div>
+          <div>
+            <Input aria-label="Date of Birth" name="dob" placeholder="02/07/86" />
+          </div>
+        </section>
+
+        <Divider className="my-10" soft />
+
+        <section className="grid gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div className="space-y-1">
+            <Subheading>Password</Subheading>
+          </div>
+          <div>
+            <Input type="password" aria-label="Password" name="password" />
+          </div>
+          <div className="space-y-1">
+            <Subheading>Confirm Password</Subheading>
+          </div>
+          <div>
+            <Input type="password" aria-label="Confirm Password" name="password_confirmation" />
+          </div>
+        </section>
+
+        <Divider className="my-10" soft />
+
+        <div className="flex justify-end gap-4">
+          <Button type="reset" plain>
+            Reset
+          </Button>
+          <Button type="submit">Register</Button>
+        </div>
+      </form>
+    </>
   )
 }
